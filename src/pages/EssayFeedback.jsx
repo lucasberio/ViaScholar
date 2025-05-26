33c74c4cd8e580e226501e65aa55d5304ad87457
lucasberio <lucasberio815@gmail.com>
2025-05-26 15:56:34 -0500

    icon and title for essay feedback

--- src/pages/EssayFeedback.jsx
+++ src/pages/EssayFeedback.jsx
@@ -1,12 +1,14 @@
-// src/pages/EssayFeedback.jsx
 import React from 'react';
 import './EssayFeedback.css';
+import { MessageSquare } from 'lucide-react';
 
 export default function EssayFeedback() {
   return (
     <div className="essay-feedback page">
-      <h2>Essay Feedback</h2>
-      {/* TODO: List essays and their feedback here */}
+      <header className='essay-header'>
+        <MessageSquare size={20} />
+        <h2>Essay Feedback</h2>
+    </header>
     </div>
   );
 }
